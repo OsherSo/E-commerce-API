6680560d6c7f55d19ce13f6019a74d57690add5d
Osher Solimany <solimani.osher@gmail.com>
2023-12-03 23:10:19 +0200

    Add basic express server

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+const express = require('express');
+
+const notFound = require('./middleware/notFound');
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
