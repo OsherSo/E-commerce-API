7b86b863121a9d417cbd3d2fd15ddb569c41c326
Osher Solimany <solimani.osher@gmail.com>
2023-12-04 10:33:35 +0200

    Add jwt create and verify

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,18 @@
 const { StatusCodes } = require('http-status-codes');
 
 const User = require('../models/User');
+const { createJWT, verifyToken } = require('../utils/jwt');
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
+  const user = await User.create({ name, email, password });
+  const tokenUser = { userId: user._id, name: user.name, role: user.role };
+  const token = createJWT(tokenUser);
 
   res.status(StatusCodes.CREATED).json({
-    user,
+    tokenUser,
+    token,
   });
 };
 
