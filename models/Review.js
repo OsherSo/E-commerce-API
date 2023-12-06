dc5bd4918591386c8aa32ad9a1eb4fdda6f0ca97
Osher Solimany <solimani.osher@gmail.com>
2023-12-06 13:33:23 +0200

    Add basic review model

---
+++ models/Review.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide title'],
+      maxLength: [50, ''],
+    },
+    comment: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide comment'],
+      maxLength: [200, ''],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, ''],
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Product',
+      required: [true, ''],
+    },
+  },
+  { timestamps: true }
+);
+
+reviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model('Review', reviewSchema);
